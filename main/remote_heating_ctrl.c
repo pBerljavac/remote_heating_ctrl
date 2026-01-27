a0029cc178d3e2d90565c075f440b8fd260c5b23
pBerljavac <paolo.berljavac@gmail.com>
2026-01-27 21:21:38 +0100

    adc implementation, buildable

--- main/remote_heating_ctrl.c
+++ main/remote_heating_ctrl.c
@@ -24,13 +24,27 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_system.h"
+
+#include "esp_adc/adc_continuous.h"
 
 #include "esp_http_client.h"
 
+//HTTP CONFIG
 #define MAX_HTTP_RECV_BUFFER 512
 #define MAX_HTTP_OUTPUT_BUFFER 2048
-static const char *TAG = "HTTP_CLIENT";
+
+//ADC READ CONFIG
+#define ADC_READ_UNIT                    ADC_UNIT_1
+#define ADC_READ_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
+#define ADC_READ_ATTEN                   ADC_ATTEN_DB_12
+#define ADC_READ_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+#define ADC_READ_LEN 256
+
+static TaskHandle_t s_task_handle;
+
+static const char *TAG = "HTTP_CLIENT"; //hmm
 
 /* Root cert for howsmyssl.com, taken from howsmyssl_com_root_cert.pem
 
@@ -1094,7 +1108,6 @@ static void http_test_task(void *pvParameters)
 #endif
 }
 
-// TO DO: Should only run at requested change or change from heater side
 static void http_test_task_mod(void *pvParameters)
 {
     bool bEnaRemoteCtrl = false;
@@ -1153,8 +1166,160 @@ static void http_test_task_mod(void *pvParameters)
 #endif
 }
 
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
+
+static void continuous_adc_init(adc_channel_t channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
+{
+    adc_continuous_handle_t handle = NULL;
+
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = ADC_READ_LEN,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = 20 * 1000,
+        .conv_mode = ADC_READ_CONV_MODE,
+    };
+
+    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
+    dig_cfg.pattern_num = channel_num;
+    for (int i = 0; i < channel_num; i++) {
+        adc_pattern[i].atten = ADC_READ_ATTEN;
+        adc_pattern[i].channel = channel & 0x7;
+        adc_pattern[i].unit = ADC_READ_UNIT;
+        adc_pattern[i].bit_width = ADC_READ_BIT_WIDTH;
+
+        ESP_LOGI(TAG, "adc_pattern[%d].atten is :%"PRIx8, i, adc_pattern[i].atten);
+        ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
+        ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
+    }
+    dig_cfg.adc_pattern = adc_pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+    *out_handle = handle;
+}
+
+void adc_read(void *pvParameters)
+{
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+    uint8_t result[ADC_READ_LEN] = {0};
+    memset(result, 0xcc, ADC_READ_LEN);
+
+    static adc_channel_t channel = ADC_CHANNEL_3;
+
+    adc_continuous_handle_t handle = NULL;
+    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle);
+
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(handle));
+
+
+    while (1)
+    {
+
+        /**
+         * This is to show you the way to use the ADC continuous mode driver event callback.
+         * This `ulTaskNotifyTake` will block when the data processing in the task is fast.
+         * However in this example, the data processing (print) is slow, so you barely block here.
+         *
+         * Without using this event callback (to notify this task), you can still just call
+         * `adc_continuous_read()` here in a loop, with/without a certain block timeout.
+         */
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        while (1)
+        {
+            ret = adc_continuous_read(handle, result, ADC_READ_LEN, &ret_num, 0);
+            if (ret == ESP_OK)
+            {
+                // ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+
+                adc_continuous_data_t parsed_data[ret_num / SOC_ADC_DIGI_RESULT_BYTES];
+                uint32_t num_parsed_samples = 0;
+
+                esp_err_t parse_ret = adc_continuous_parse_data(handle, result, ret_num, parsed_data, &num_parsed_samples);
+                if (parse_ret == ESP_OK)
+                {
+                    // for (int i = 0; i < num_parsed_samples; i++)
+                    // {
+                    //     if (parsed_data[i].valid)
+                    //     {
+                    //         float roomTemp = (float)(parsed_data[i].raw_data) * 0.1f;
+
+                    //         ESP_LOGI(TAG, "ADC%d, Channel: %d, Room temperature: %.01f",
+                    //                  parsed_data[i].unit + 1,
+                    //                  parsed_data[i].channel,
+                    //                  roomTemp);
+                    //     }
+                    //     else
+                    //     {
+                    //         ESP_LOGW(TAG, "Invalid data [ADC%d_Ch%d_%"PRIu32"]",
+                    //                  parsed_data[i].unit + 1,
+                    //                  parsed_data[i].channel,
+                    //                  parsed_data[i].raw_data);
+                    //     }
+                    // }
+
+                    //Rough reading of just the last data sample
+                    if (parsed_data[num_parsed_samples - 1].valid)
+                    {
+                        float roomTemp = (float)(parsed_data[num_parsed_samples - 1].raw_data) * 0.1f;
+
+                        ESP_LOGI(TAG, "ADC%d, Channel: %d, Room temperature: %.01f deg C",
+                                    parsed_data[num_parsed_samples - 1].unit + 1,
+                                    parsed_data[num_parsed_samples - 1].channel,
+                                    roomTemp);
+                    }
+                    else
+                    {
+                        ESP_LOGW(TAG, "Invalid data [ADC%d_Ch%d_%"PRIu32"]",
+                                    parsed_data[num_parsed_samples - 1].unit + 1,
+                                    parsed_data[num_parsed_samples - 1].channel,
+                                    parsed_data[num_parsed_samples - 1].raw_data);
+                    }
+                }
+                else
+                {
+                    ESP_LOGE(TAG, "Data parsing failed: %s", esp_err_to_name(parse_ret));
+                }
+
+                /**
+                 * Because printing is slow, so every time you call `ulTaskNotifyTake`, it will immediately return.
+                 * To avoid a task watchdog timeout, add a delay here. When you replace the way you process the data,
+                 * usually you don't need this delay (as this task will block for a while).
+                 */
+                vTaskDelay(CONFIG_DISPLAY_PERIOD / portTICK_PERIOD_MS);
+            }
+            else if (ret == ESP_ERR_TIMEOUT)
+            {
+                //We try to read `EXAMPLE_READ_LEN` until API returns timeout, which means there's no available data
+                break;
+            }
+        }
+    }
+
+    ESP_ERROR_CHECK(adc_continuous_stop(handle));
+    ESP_ERROR_CHECK(adc_continuous_deinit(handle));
+
+    uint8_t tSnsr = 0;
+}
+
 void app_main(void)
 {
+    //Memory initialization
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
@@ -1162,6 +1327,7 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
+    //Network device task init
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
@@ -1175,6 +1341,9 @@ void app_main(void)
 #if CONFIG_IDF_TARGET_LINUX
     http_test_task_mod(NULL);
 #else
-    xTaskCreate(&http_test_task_mod, "http_test_task_mod", 8192, NULL, 5, NULL);
+    //WiFi operation on core 0
+    xTaskCreatePinnedToCore(&http_test_task_mod, "http_test_task_mod", 8192, NULL, 5, NULL, 0);
+    //Sensor operation on core 1
+    xTaskCreatePinnedToCore(&adc_read, "adc_read", 4096, NULL, 3, NULL, 1);
 #endif
 }
